14d8bc094087a3156a62765eb5fe03a5ef22b985
GA <puppy970802@gmail.com>
2020-02-25 02:14:20 +0900

    프로그래머스

---
+++ 시저 암호.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+string solution(string s, int n) {
+	string answer = "";
+	for (int i = 0; i < s.length(); i++) {
+		if (s[i] == ' ') //공백일 경우 그대로
+			answer += " ";
+		else {
+			//소문자, 대문자 범위 벗어난 경우
+			if (((s[i] + n > 122)) || ((s[i] < 91) && (s[i] + n >= 91)))
+				answer += (s[i] + n - 26);
+			else
+				answer += (s[i] + n);
+		}
+	}
+	return answer;
+}
