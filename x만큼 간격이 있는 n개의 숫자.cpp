82b23b2f24b082cdbacb00b889bf3b5aad8c1672
GA <puppy970802@gmail.com>
2020-03-08 23:49:13 +0900

    프로그래머스
    [프로그래머스] x만큼 간격이 있는 n개의 숫자

---
+++ x만큼 간격이 있는 n개의 숫자.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<long long> solution(int x, int n) {
+	vector<long long> answer;
+	long long sum = x;
+	for (int i = 0; i < n; i++) {
+		answer.push_back(sum);
+		sum += x;
+	}
+	return answer;
+}
