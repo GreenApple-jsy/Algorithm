42fec4fa3f2219ca89091a85f98e432827e08241
GA <puppy970802@gmail.com>
2020-03-22 00:33:33 +0900

    프로그래머스
    [프로그래머스] 전화번호 목록

---
+++ 전화번호 목록.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+bool solution(vector<string> phone_book) {
+    bool answer = true;
+    for (int i = 0; i < phone_book.size(); i++)
+    {
+        for (int j = 0; j < i; j++) { //이전 전화번호들과 비교
+            int cmp_length = (phone_book[i].length() < phone_book[j].length()) ? phone_book[i].length() : phone_book[j].length();
+            if (phone_book[i].substr(0, cmp_length) == phone_book[j].substr(0, cmp_length)) {
+                return false;
+            }
+        }
+    }
+    return answer;
+}
