4b5c60ba213a7de7c4254f5965428a53d9211c50
GA <puppy970802@gmail.com>
2020-04-14 17:57:55 +0900

    프로그래머스
    [프로그래머스] 쇠막대기

---
+++ 쇠막대기.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(string arrangement) {
+	int answer = 0; //쇠막대기 수 + 커팅 횟수
+	int currentStickN = 0; //레이저가 자르게 될 쇠막대기 갯수
+	for (int i = 0; i < arrangement.length() - 1; i++) {
+		if (arrangement[i] == '(') {
+			if (arrangement[i + 1] == ')') { //레이저인 경우
+				answer += currentStickN;
+			}
+			else { //쇠막대기 시작 위치인 경우
+				answer++;
+				currentStickN++;
+			}
+		}
+		else if ((arrangement[i] == ')') && (arrangement[i - 1] == ')')) { //쇠막대기 끝나는 위치인 경우
+			currentStickN--;
+		}
+	}
+	return answer;
+}
