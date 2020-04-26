592f98fba4538598cd8a25b5d46cd5e7a07cf2f9
GA <puppy970802@gmail.com>
2020-04-26 23:43:34 +0900

    프로그래머스
    [프로그래머스] 하샤드 수

--- 하샤드 수.cpp
+++ 하샤드 수.cpp
@@ -1,1 +1,17 @@
+#include <string>
+#include <vector>
+using namespace std;
 
+bool solution(int x) {
+	int sum = 0;
+	int t = x;
+	while (t >= 10) {
+		sum += t % 10; //각 자리수 더하기
+		t /= 10;
+	}
+	sum += t;
+	if (x % sum == 0)
+		return true;
+	else
+		return false;
+}
