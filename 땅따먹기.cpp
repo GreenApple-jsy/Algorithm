7d3a7a95c5441581c0418bf601cfce8a3352075f
GA <puppy970802@gmail.com>
2020-05-14 17:54:08 +0900

    프로그래머스
    [프로그래머스] 땅따먹기

--- 땅따먹기.cpp
+++ 땅따먹기.cpp
@@ -1,18 +1,23 @@
-#include <iostream>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
-int solution(vector<vector<int> > land)
-{
-	for (int i = 1; i < land.size(); i++) {     
-		for (int j = 0; j < 4; j++)
-			land[i][j] += previous_max(land[i - 1], j);
-	}
-	return *max_element((land.back()).begin(), (land.back()).end());
+int previous_max(vector<int> &previous_line, int index) {
+	if (index == 0)
+		return *max_element(previous_line.begin() + 1, previous_line.end());
+	else if (index == 1)
+		return (previous_line[0] > *max_element(previous_line.begin() + 2, previous_line.end())) ? previous_line[0] : *max_element(previous_line.begin() + 2, previous_line.end());
+	else if (index == 2)
+		return (previous_line[3] > *max_element(previous_line.begin(), previous_line.begin() + 2)) ? previous_line[3] : *max_element(previous_line.begin(), previous_line.begin() + 2);
+	else if (index == 3)
+		return *max_element(previous_line.begin(), previous_line.end() - 1);
 }
 
-int main() {
-	cout << solution({ {1,2,3,5},{5,6,7,8},{4,3,2,1} });
-	system("pause");
+int solution(vector<vector<int> > land)
+{
+	for (int i = 1; i < land.size(); i++) {
+		for (int j = 0; j < 4; j++)
+			land[i][j] += previous_max(land[i - 1], j); //이전 행에서 같은 열을 제외한 원소들 중 가장 큰 원소를 더함
+	}
+	return *max_element((land.back()).begin(), (land.back()).end());
 }
