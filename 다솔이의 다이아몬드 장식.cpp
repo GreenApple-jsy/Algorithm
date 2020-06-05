afe92ad9032ba77a74c439cd9d32c9550aa9f2d7
GA <puppy970802@gmail.com>
2020-06-05 18:53:10 +0900

    SWEA
    [SWEA 4751] 다솔이의 다이아몬드 장식

--- 다솔이의 다이아몬드 장식.cpp
+++ 다솔이의 다이아몬드 장식.cpp
@@ -4,17 +4,42 @@
 using namespace std;
 
 string FirstAndFifthLine(int stringLength) {
+	string s;
 	for (int i = 1; i <= ((5 * stringLength) - stringLength + 1); i++) {
-		
+		if ((i - 3) % 4 == 0)
+			s += "#";
+		else
+			s += ".";
 	}
+	return s + "\n";
 }
 
 string ThirdLine(string inputString) {
-
+	int stringLength = inputString.length();
+	string s;
+	int index = 0;
+	for (int i = 1; i <= ((5 * stringLength) - stringLength + 1); i++) {
+		if ((i - 1) % 4 == 0)
+			s += "#";
+		else if ((i - 3) % 4 == 0) {
+			s += inputString[index];
+			index++;
+		}
+		else
+			s += ".";
+	}
+	return s + "\n";
 }
 
 string SecondAndFourthLine(int stringLength) {
-
+	string s;
+	for (int i = 1; i <= ((5 * stringLength) - stringLength + 1); i++) {
+		if (i % 2 == 0)
+			s += "#";
+		else
+			s += ".";
+	}
+	return s + "\n";
 }
 
 int main() {
