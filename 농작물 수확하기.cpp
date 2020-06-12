63efb76964000251820bccee6a237886f6867a85
GA <puppy970802@gmail.com>
2020-06-12 09:21:08 +0900

    SWEA
    [SWEA 2805] 농작물 수확하기

--- 농작물 수확하기.cpp
+++ 농작물 수확하기.cpp
@@ -1,20 +1,27 @@
 #include <string>
-#include <vector>
 #include <iostream>
 using namespace std;
 
 int main() {
-	vector <vector <int>> farm;
 	string answer;
-	int T, N;
+	int T, N, sum, current, addN, temp = 0;
 	cin >> T;
 	for (int i = 1; i <= T; i++) {
 		cin >> N;
-		farm.resize(N);
+		current = N / 2;
+		sum = 0;
+		addN = -1;
 		for (int j = 0; j < N; j++) {
-			farm[j].resize(N);
-			
+			for (int k = 1; k <= N; k++) {
+				scanf("%1d", &temp);
+				if ((k > current) && (k <= N - current))
+					sum += temp;
+			}
+			if (current == 0)
+				addN = 1;
+			current += addN;
 		}
+		answer += "#" + to_string(i) + " " + to_string(sum) + "\n";
 	}
 	cout << answer;
 	return 0;
