491f38920873e78d0f7baa3225a65e9fd51c7419
GA <puppy970802@gmail.com>
2020-06-16 17:52:32 +0900

    SWEA
    [SWEA 3456] 직사각형 길이 찾기

--- 직사각형 길이 찾기.cpp
+++ 직사각형 길이 찾기.cpp
@@ -1,8 +1,20 @@
-#include <iostream>
 #include <string>
+#include <iostream>
 using namespace std;
 
-int main(){
-
-return 0;
+int main() {
+	int T, a, b, c;
+	string answer;
+	cin >> T;
+	for (int i = 1; i <= T; i++) {
+		cin >> a >> b >> c;
+		if (a == b)
+			answer += "#" + to_string(i) + " " + to_string(c) + "\n";
+		else if (a == c)
+			answer += "#" + to_string(i) + " " + to_string(b) + "\n";
+		else if (b == c)
+			answer += "#" + to_string(i) + " " + to_string(a) + "\n";
+	}
+	cout << answer;
+	return 0;
 }
