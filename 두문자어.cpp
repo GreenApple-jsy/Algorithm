9724ae0b5d06d69a496cae0654d3403d7ef719bb
GA <puppy970802@gmail.com>
2020-06-18 18:44:33 +0900

    SWEA
    [SWEA 8741] 두문자어

--- 두문자어.cpp
+++ 두문자어.cpp
@@ -7,15 +7,15 @@ int main() {
 	int T;
 	string input, answer = "";
 	cin >> T;
-	getline(cin, input);
+	getline(cin, input); //줄바꿈 문자 무시
 	for (int i = 1; i <= T; i++) {
 		answer += "#" + to_string(i) + " ";
-		getline(cin, input);
+		getline(cin, input); //한줄 문자열 입력받기
 		c = toupper(input[0]);
 		for (int j = 1; j < input.length() - 1; j++) {
 			if (input[j] == ' ') {
 				answer += c;
-				c = toupper(input[j + 1]);
+				c = toupper(input[j + 1]); //단어의 첫문자를 대문자로 변환하여 저장
 			}	
 		}
 		answer += string(1,c) + "\n";
