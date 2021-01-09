bd670fb4762b4fafa5013d0c1b2f8c60455a5a12
GA <puppy970802@gmail.com>
2021-01-09 23:41:21 +0900

    Create 방금그곡.cpp

---
+++ 방금그곡.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+//음악 제목, 재생이 시작되고 끝난 시각, 악보
+//C, C#, D, D#, E, F, F#, G, G#, A, A#, B
+//음악은 반드시 처음부터 재생되며 음악 길이보다 재생된 시간이 길 때는 음악이 끊김 없이 처음부터 반복해서 재생
+//조건이 일치하는 음악이 여러 개일 때에는 라디오에서 재생된 시간이 제일 긴 음악
+//재생된 시간도 같을 경우 먼저 입력된 음악
+//조건이 일치하는 음악이 없을 때에는 “(None)”
+
+//음악이 시작한 시각, 끝난 시각, 음악 제목, 악보 정보
+string solution(string m, vector<string> musicinfos) {
+    
+}
