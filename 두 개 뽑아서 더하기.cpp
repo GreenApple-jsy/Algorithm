0feab0243c85bcaa7379110c7672fa16da4fa185
GA <puppy970802@gmail.com>
2021-01-18 23:20:54 +0900

    프로그래머스
    [프로그래머스] 두 개 뽑아서 더하기

---
+++ 두 개 뽑아서 더하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(vector<int> numbers) {
+	vector<int> answer;
+	for (int i = 0; i < numbers.size(); i++) {
+		for (int j = i + 1; j < numbers.size(); j++)
+			answer.push_back(numbers[i] + numbers[j]);
+	}
+	sort(answer.begin(), answer.end());
+	answer.erase(unique(answer.begin(), answer.end()), answer.end()); //중복 제거
+	return answer;
+}
