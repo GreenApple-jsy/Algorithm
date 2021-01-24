2a8a5e066762af36d056db991094f59f66df949e
GA <puppy970802@gmail.com>
2021-01-24 23:50:57 +0900

    Update 경주로 건설.cpp

--- 경주로 건설.cpp
+++ 경주로 건설.cpp
@@ -1,82 +1,52 @@
-#include <string>
 #include <vector>
-#include <iostream>
+#include <queue>
 using namespace std;
 
-int min_coast;
-vector<vector<int>> g_board;
-int board_size;
-
-// 0 : 오른쪽, 1 : 아래, 2 : 왼쪽, 3 : 위
-void find_route(int x, int y, int direction, int current_cost, vector<vector<bool>> board_visit_check) {
-    cout << x << " , " << y <<  "   //" << current_cost << endl;
-    if ((x == g_board.size() - 1) && (y == g_board.size() - 1)) {
-        if (current_cost < min_coast)
-            min_coast = current_cost;
-        return;
-    }
-    //오른쪽
-    if ((y + 1 < board_size) && (board_visit_check[x][y + 1] == false) && (g_board[x][y + 1] == 0)) {
-        int temp_coast = current_cost;
-        if (direction != 0)
-            temp_coast += 500;
-        temp_coast += 100;
-        board_visit_check[x][y + 1] = true;
-        find_route(x, y + 1, 0, temp_coast, board_visit_check);
-        board_visit_check[x][y + 1] = false;
-    }
-    //아래
-    if ((x + 1 < board_size) && (board_visit_check[x + 1][y] == false) && (g_board[x + 1][y] == 0)) {
-        int temp_coast = current_cost;
-        if (direction != 1)
-            temp_coast += 500;
-        temp_coast += 100;
-        board_visit_check[x + 1][y] = true;
-        find_route(x + 1, y, 1, temp_coast, board_visit_check);
-        board_visit_check[x + 1][y] = false;
-    }
-    //왼쪽
-    if ((y - 1 >= 0) && (board_visit_check[x][y - 1] == false) && (g_board[x][y - 1] == 0)) {
-        int temp_coast = current_cost;
-        if (direction != 2)
-            temp_coast += 500;
-        temp_coast += 100;
-        board_visit_check[x][y - 1] = true;
-        find_route(x, y - 1, 2, temp_coast, board_visit_check);
-        board_visit_check[x][y - 1] = false;
-    }
-    //위
-    if ((x - 1 >= 0) && (board_visit_check[x - 1][y] == false) && (g_board[x - 1][y] == 0)) {
-        int temp_coast = current_cost;
-        if (direction != 3)
-            temp_coast += 500;
-        temp_coast += 100;
-        board_visit_check[x - 1][y] = true;
-        find_route(x - 1, y, 3, temp_coast, board_visit_check);
-        board_visit_check[x - 1][y] = false;
-    }
-    return;
-}
+struct Car {
+    int x, y;
+    int current_direction; //0:왼쪽, 1:오른쪽, 2:위, 3:아래
+    int cost;
+};
 
 int solution(vector<vector<int>> board) {
-    min_coast = 99999999;
-    g_board = board;
-    board_size = board.size();
-    vector <bool> temp(board_size, false);
-    vector<vector<bool>> board_visit_check;
-    for (int i = 0; i < board_size; i++)
-        board_visit_check.push_back(temp);
-
+    board[0][0] = -1;
+    int N = board.size();
+    int min_cost = 99999999;
+    int mx[] = { 0, 0, -1, 1 }, my[] = { -1, 1, 0, 0 };
+    queue <Car> q;
     if (board[0][1] == 0) {
-        board_visit_check[0][1] = true;
-        find_route(0, 1, 0, 100, board_visit_check);
-        board_visit_check[0][1] = false;
+        q.push({ 0, 1, 1, 100 });
+        board[0][1] = 100;
     }
-        
     if (board[1][0] == 0) {
-        board_visit_check[1][0] = true;
-        find_route(1, 0, 1, 100, board_visit_check);
-        board_visit_check[1][0] = false;
+        q.push({ 1, 0, 3, 100 });
+        board[1][0] = 100;
+    }   
+    while (!q.empty()) {
+        Car c = q.front();
+        q.pop();
+        if ((c.x == N - 1) && (c.y == N - 1)) {
+            if (c.cost < min_cost)
+                min_cost = c.cost;
+            continue;
+        }
+        for (int i = 0; i < 4; i++) {
+            int new_x = c.x + mx[i];
+            int new_y = c.y + my[i];
+
+            if ((new_x < 0) || (new_y < 0) || (new_x >= N) || (new_y >= N) || (board[new_x][new_y] == 1))
+                continue;
+
+            int new_cost = c.cost + 100;
+            if (c.current_direction != i)
+                new_cost += 500;
+
+            if ((board[new_x][new_y] == 0) || (new_cost <= board[new_x][new_y])) {
+                Car d{ new_x, new_y, i, new_cost};
+                q.push(d);
+                board[new_x][new_y] = new_cost;
+            }
+        }
     }
-    return min_coast;
+    return min_cost;
 }
