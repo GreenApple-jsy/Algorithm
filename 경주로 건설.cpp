f7b0fcfdf811a5ea004986dcc530edb25ddd1f72
GA <puppy970802@gmail.com>
2021-01-25 00:31:14 +0900

    프로그래머스
    [프로그래머스, 2020 카카오 인턴십] 경주로 건설

--- 경주로 건설.cpp
+++ 경주로 건설.cpp
@@ -5,23 +5,25 @@ using namespace std;
 struct Car {
     int x, y;
     int current_direction; //0:왼쪽, 1:오른쪽, 2:위, 3:아래
-    int cost;
+    int cost; //누적 코스트
 };
 
 int solution(vector<vector<int>> board) {
     board[0][0] = -1;
     int N = board.size();
     int min_cost = 99999999;
-    int mx[] = { 0, 0, -1, 1 }, my[] = { -1, 1, 0, 0 };
+    int mx[] = { 0, 0, -1, 1 }, my[] = { -1, 1, 0, 0 }; //왼쪽, 오른쪽, 위, 아래
     queue <Car> q;
-    if (board[0][1] == 0) {
+
+    if (board[0][1] == 0) { //장애물 없을 경우 오른쪽 방향 스타트 추가
         q.push({ 0, 1, 1, 100 });
         board[0][1] = 100;
     }
-    if (board[1][0] == 0) {
+    if (board[1][0] == 0) { //장애물 없을 경우 아래 방향 스타트 추가
         q.push({ 1, 0, 3, 100 });
         board[1][0] = 100;
     }   
+
     while (!q.empty()) {
         Car c = q.front();
         q.pop();
@@ -34,6 +36,7 @@ int solution(vector<vector<int>> board) {
             int new_x = c.x + mx[i];
             int new_y = c.y + my[i];
 
+            //범위 초과이거나 장애물이 있는 경우
             if ((new_x < 0) || (new_y < 0) || (new_x >= N) || (new_y >= N) || (board[new_x][new_y] == 1))
                 continue;
 
@@ -41,6 +44,8 @@ int solution(vector<vector<int>> board) {
             if (c.current_direction != i)
                 new_cost += 500;
 
+            // 장애물이 없고 방문한 적이 없거나, 새로 계산한 가격이 이전 기록한 값보다 작거나 같은 경우
+            // queue에 추가 및 가격 갱신
             if ((board[new_x][new_y] == 0) || (new_cost <= board[new_x][new_y])) {
                 Car d{ new_x, new_y, i, new_cost};
                 q.push(d);
