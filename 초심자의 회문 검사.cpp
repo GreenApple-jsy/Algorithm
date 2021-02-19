4cbd339b487cd8e3c6d04916f6dcf62d711f1ce0
GA <puppy970802@gmail.com>
2021-02-20 00:58:03 +0900

    SWEA
    [SWEA 1989] 초심자의 회문 검사

--- 초심자의 회문 검사.cpp
+++ 초심자의 회문 검사.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 
 int main() {
-  return 0;
+	int T;
+	cin >> T;
+	for (int i = 1; i <= T; i++) {
+		bool is_palindrome = true;
+		string S;
+		cin >> S;
+		int sSize = S.length();
+		for (int j = 0; j <= sSize / 2; j++) {
+			if (S[j] != S[sSize - j - 1]) {
+				is_palindrome = false;
+				break;
+			}
+		}
+		cout << "#" << i << " " << is_palindrome << endl;
+	}
+	return 0;
 }
