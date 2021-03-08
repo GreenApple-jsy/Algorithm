7bfd1bf77a7000829aa0b8bf15081e4085da2c56
GA <puppy970802@gmail.com>
2021-03-09 00:15:01 +0900

    SWEA
    [SWEA 9317] 석찬이의 받아쓰기

--- 석찬이의 받아쓰기.cpp
+++ 석찬이의 받아쓰기.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
-  return 0;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	int TC, N, answer;
+	char A[100000], B[100000];
+	cin >> TC;
+	for (int i = 1; i <= TC; i++) {
+		cin >> N >> A >> B;
+		answer = N;
+		for (int j = 0; j < N; j++) {
+			if (A[j] != B[j])
+				answer--;
+		}
+		cout << "#" << i << " " << answer << "\n";
+	}
+	return 0;
 }
