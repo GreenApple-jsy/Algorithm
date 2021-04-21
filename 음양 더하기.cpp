c79c51232c969f75a886727209b2028ea0199d74
GA <puppy970802@gmail.com>
2021-04-22 00:02:57 +0900

    프로그래머스
    [프로그래머스 / 월간 코드 챌린지] 음양 더하기

---
+++ 음양 더하기.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(vector<int> absolutes, vector<bool> signs) {
+    int answer = 0;
+    for (int i = 0; i < absolutes.size(); i++)
+        signs[i] == true ? answer += absolutes[i] : answer -= absolutes[i];
+    return answer;
+}
