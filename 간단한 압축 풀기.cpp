5eff0ccffdab14f4e3170057794f279b24d19003
GA <puppy970802@gmail.com>
2021-09-11 00:39:34 +0900

    SWEA
    [SWEA 1946] 간단한 압축 풀기

--- 간단한 압축 풀기.cpp
+++ 간단한 압축 풀기.cpp
@@ -4,6 +4,23 @@
 using namespace std;
 
 int main() {
-
+	int T, N;
+	cin >> T;
+	for (int i = 1; i <= T; i++) {
+		cin >> N;
+		string s = "";
+		int n = 0;
+		for (int j = 1; j <= N; j++) {
+			char Ci; int Ki;
+			cin >> Ci >> Ki;
+			for (int k = 1; k <= Ki; k++) {
+				s += Ci;
+				n++;
+				if (n % 10 == 0)
+					s += '\n';
+			}	
+		}
+		cout << "#" << i << "\n" << s << endl;
+	}
 	return 0;
 }
