6a9cc66d0d48ee810036489ff9601af809c7ef8e
GA <puppy970802@gmail.com>
2021-09-27 00:07:13 +0900

    SWEA
    [SWEA 1285] 아름이의 돌 던지기

--- 아름이의 돌 던지기.cpp
+++ 아름이의 돌 던지기.cpp
@@ -2,6 +2,23 @@
 using namespace std;
 
 int main() {
+	int T, N, dist;
+	cin >> T;
 
+	for (int i = 1; i <= T; ++i) {
+		cin >> N;
+		int min_dist = 100000, same_dist_count = 0; //가장 가까운 위치, 같은 거리인 돌 개수 
+		for (int j = 0; j < N; ++j) {
+			cin >> dist;
+			if (dist < 0) dist *= -1;
+			if (dist < min_dist) {
+				min_dist = dist;
+				same_dist_count = 1;
+			}
+			else if (dist == min_dist)
+				same_dist_count++;
+		}
+		cout << "#" << i << " " << min_dist << " " << same_dist_count << endl;
+	}
 	return 0;
 }
